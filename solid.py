af76e40c1dafd3be07040a39e424c90e5ae49f9a
gulshanyadav <gulshany01@gmail.com>
2024-06-11 23:41:55 +0530

    initial commit

---
+++ solid.py
@@ -0,0 +1,7 @@
+# Solid Principle:
+#     s : single responsibility principle
+#     o : open for extension and closed for modification
+#     l : liskov substitution principle
+#     i : interface seggregation principle
+#     d : dependency inversion principle
+
